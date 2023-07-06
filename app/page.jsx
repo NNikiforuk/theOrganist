f90f32dfa6db5c0dec6ae285cb083f8e71875ff8
Natalia <natnikiforuk@gmail.com>
2023-07-06 11:27:25 +0200

    Home page styling

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,14 @@
 const Home = () => {
 	return (
 		<section className="w-full flex-center flex-col">
-			<h1 className="head_text">Whereas recognition</h1>
+			<h1 className="head_text text-center">
+				<span className="logo_text">theOrganist</span>
+				<p className="desc">
+					Plan your track list for ceremonies
+				</p>
+
+				{/* Feed */}
+			</h1>
 		</section>
 	);
 };
---
+++ components/PromptCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PromptCard = () => {
+  return (
+    <div>
+      prompt card
+    </div>
+  )
+}
+
+export default PromptCard
