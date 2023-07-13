ff0a6c771e34e91538438f666988eadda7e1bd05
Natalia <natnikiforuk@gmail.com>
2023-07-13 19:39:59 +0200

    Sorting songs by the tag

--- components/Song_list.jsx
+++ components/Song_list.jsx
@@ -2,9 +2,13 @@ import Song from "./Song";
 
 const Song_list = ({ songs }) => {
 
+	const sortSongs = [...songs].sort((a, b) => (a.tag > b.tag ? 1 : -1));
+
 	return (
 		<div className="song_list">
-			{songs.map((song) => <Song key={song._id} song={song} />)}
+			{sortSongs.map((song) => (
+				<Song key={song._id} song={song} />
+			))}
 		</div>
 	);
 };
