75a50531092a0114d2c68f579f2bf45fe6857b2f
Natalia <natnikiforuk@gmail.com>
2023-07-17 19:47:40 +0200

    Adding favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,11 +2,15 @@ import { Work_Sans } from "next/font/google";
 
 import Nav from "@components/Nav";
 import Provider from "@components/Provider";
+
 import "@styles/globals.css";
 
 export const metadata = {
 	title: "Organ track list",
 	description: "Arrange your own track list",
+	icons: {
+		icon: "icon.ico"
+	}
 };
 
 const workSans = Work_Sans({
@@ -18,6 +22,9 @@ const workSans = Work_Sans({
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
+			<head>
+				<link rel="icon" href="icon.ico" />
+			</head>
 			<body className={`${workSans.className}`}>
 				<Provider>
 					<main className="main flex-col">
